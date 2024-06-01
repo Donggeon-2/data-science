1519f42195d2b1e012f069305acc8670be660c08
Donggeon-2 <donggeon298@naver.com>
2024-06-01 19:04:58 +0900

    knn algorithm

--- knn-algorithm.py
+++ knn-algorithm.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from collections import Counter
 
 def load_data(file_path):
     """
@@ -50,18 +51,24 @@ def visualize_data(data, new_data=None):
 
     plt.show()
 
-def classify_new_data(new_data):
+def knn_classify(data, new_data, k=3):
     """
-    A function that classifies new data in a scatterplot
-
+    K-Nearest Neighbors classification
     """
-    new_ratio = new_data['TotalKg'] / new_data['BodyweightKg']
-    if new_ratio.values >= 6.75:
-        return "Pro"
-    elif new_ratio.values >= 4.72:
-        return "Amateur"
-    else:
-        return "Beginner"
+    # Calculate distances between new_data and all data points
+    distances = np.sqrt(np.sum((data[['BodyweightKg', 'TotalKg']] - new_data.values) ** 2, axis=1))
+    
+    # Get the indices of the k nearest neighbors
+    nearest_indices = distances.argsort()[:k]
+    
+    # Get the classes of the nearest neighbors
+    nearest_classes = data.iloc[nearest_indices]['Class']
+    
+    # Count the occurrences of each class
+    class_counts = nearest_classes.value_counts()
+    
+    # Return the class with the highest count
+    return class_counts.idxmax()
 
 # Load data
 data = load_data("openpowerlifting.csv")
@@ -70,5 +77,6 @@ data = load_data("openpowerlifting.csv")
 new_data = pd.DataFrame({'BodyweightKg': [65], 'TotalKg': [210]})
 visualize_data(data, new_data=new_data)
 
-# Classification new data
-print("New Data is classified as:", classify_new_data(new_data))
+# Classification new data using KNN algorithm
+knn_class = knn_classify(data, new_data)
+print("New Data is classified as:", knn_class)
